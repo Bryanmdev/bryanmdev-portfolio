ec40a4acca9f3c602700c6a4de6225d45e1ae174
Bryan.dev <bryanmiranda.dev@gmail.com>
2025-05-25 00:28:08 -0300

    animacoes e modo claro

--- js/script.js
+++ js/script.js
@@ -8,6 +8,7 @@ const translations = {
           "go-project": "View project",
           "futures-projects": "Soon you will see more projects here with React, Node.js, C, C#, 2D games and more.",
           "contact-me": "Contacts",
+          "legend-skill": "Technologies that I use and improve in my daily life:",
         },
         pt:{
           "my-title": "Desenvolvedor Full Stack",
@@ -18,6 +19,7 @@ const translations = {
           "go-project": "Ver projeto online",
           "futures-projects": "Em breve, você poderá ver aqui mais projetos com React, Node.js, C, C#, jogos 2D e muito mais.",
           "contact-me": "Contatos",
+          "legend-skill": "Tecnologias que utilizo e aprimoro no meu dia:",
         }
       };
 
@@ -40,4 +42,52 @@ const translations = {
   if (activeButton) {
     activeButton.classList.add("active");
   }
+  // Salva o idioma no navegador
+  localStorage.setItem("lang", lang);
       }
+
+// Ao carregar a página, busca idioma salvo
+document.addEventListener("DOMContentLoaded", () => {
+  const savedLang = localStorage.getItem("lang") || "pt" // usa "pt" se não houver valor salvo
+  setLanguage(savedLang);
+})
+
+const observer = new IntersectionObserver((entries, observer) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("fade-in");
+      observer.unobserve(entry.target); // anime só 1 vez
+    }
+  });
+}, {
+  threshold: 0.05 // 5% do elemento visível
+});
+
+// Observar todos os elementos com a classe "observe"
+document.querySelectorAll(".observe").forEach(el => {
+  observer.observe(el);
+});
+
+// Função para trocar o tema
+function toggleTheme() {
+  document.body.classList.toggle("light");
+
+  const isLight = document.body.classList.contains("light");
+  localStorage.setItem("theme", isLight ? "light" : "dark");
+
+  const themeBtnIcon = document.getElementById("theme-btn").querySelector("i");
+  themeBtnIcon.className = isLight ? "fas fa-sun" : "fas fa-moon";
+}
+
+// Verifica tema salvo ao carregar
+document.addEventListener("DOMContentLoaded", () => {
+  const savedTheme = localStorage.getItem("theme");
+  const themeBtnIcon = document.getElementById("theme-btn").querySelector("i");
+
+  if (savedTheme === "light") {
+    document.body.classList.add("light");
+    themeBtnIcon.className = "fas fa-sun";
+  }
+
+  document.getElementById("theme-btn").addEventListener("click", toggleTheme);
+});
